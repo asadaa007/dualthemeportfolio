f8aa64fb1d71a44f7e008e34c39629406fae7559
Asad Ur Rehman <114389119+asadaa007@users.noreply.github.com>
2023-02-01 21:00:05 +0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 // Nav Bar Scroll Effect
 const navbar = document.querySelector('.nav-row');
 const navAnim = document.querySelector('.nav-menu');
+const Theme = document.querySelector('.selectThm')
 window.onscroll = () => {
     if (window.scrollY > 100) {
+        Theme.classList.add('scroll-nav');
         navbar.classList.add('nav-active');
         navAnim.classList.add("nav-menu2");
     } else {
+        Theme.classList.remove('scroll-nav');
         navbar.classList.remove('nav-active');
         navAnim.classList.remove("nav-menu2");
     }
@@ -18,3 +21,27 @@ $('.slider-hero').slick({
     dots: true,
     speed:600,
 });
+
+
+// Theme
+
+function initThemeSelector() {
+    const themeSelect = document.getElementById("themeSelect");
+    const themeStylesheetLink = document.getElementById("themeStylesheetLink");
+    const currentTheme = localStorage.getItem("theme") || "lightTheme";
+
+    function activateTheme(themeName) {
+        themeStylesheetLink.setAttribute("href", `./src/css/${themeName}.css`);
+    }
+
+    themeSelect.addEventListener("change", () => {
+        activateTheme(themeSelect.value);
+        localStorage.setItem("theme", themeSelect.value);
+    });
+
+
+    themeSelect.value = currentTheme;
+    activateTheme(currentTheme);
+}
+
+initThemeSelector();
